48e5584ddea14a800021f04b98dc38b7c561b32f
veepham <111474183+veepham@users.noreply.github.com>
2022-09-12 13:06:53 -0700

    Concatenated string challenge

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,2 @@
-document.write("\"Be who you are and say what you feel," + "because those who mind don\'t matter and those matter don\'t mind.\"" + "-Dr. Seuss");
+var B = "Concatenated" + " String"
+document.write(B);
