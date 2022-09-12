f7dd91b1d3033c1cc083e45b0dc44bfd3e276bc3
veepham <111474183+veepham@users.noreply.github.com>
2022-09-12 13:58:54 -0700

    Basic JavaScript Projects submission

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,5 @@
-var Family = "The Phams", Dad = "Harry", Mom = "Samantha", Daughter ="Caitlin", Son = "Brian";
-document.write(Dad);
+window.alert("Hello")        //Utilitzing a window.alert to pop up on entry
+var Sent1 = "This is the beginning of the string";      //defining a variable with string value
+var Sent2 = " and this is the end of the string";       //defining another variable with string valu
+var A = Sent1 + Sent2       //Writing an expression to concatenate two strings
+document.write(A)       //using document.write method to return result of expression/concatenation
