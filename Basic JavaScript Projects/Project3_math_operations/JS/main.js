33ae728436a7b28c81d2cc1375917d55f08a62d0
veepham <111474183+veepham@users.noreply.github.com>
2022-09-12 17:39:20 -0700

    Subtraction operation

--- Basic JavaScript Projects/Project3_math_operations/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operations/JS/main.js
@@ -2,3 +2,7 @@ function addition_Function (){
     var addition = 2 + 2;
     document.getElementById("math").innerHTML = "2 + 2 = " + addition;
 }
+function subtraction_Function () {
+    var subtraction = 5 - 2;
+    document.getElementById("math").innerHTML = "5 - 2 = " + subtraction
+}
