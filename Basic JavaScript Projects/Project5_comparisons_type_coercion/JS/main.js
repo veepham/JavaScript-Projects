15550603f2e4b662d2d9e8149f64089146a6c248
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 16:19:48 -0700

    NaN Challenge

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,1 +1,11 @@
 document.write("20" + 4);
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+function my_Function2() {
+    document.getElementById("Test2").innerHTML = isNaN('This is a string');
+}
+function my_Function3() {
+    document.getElementById("Test3").innerHTML = isNaN('123');
+}
