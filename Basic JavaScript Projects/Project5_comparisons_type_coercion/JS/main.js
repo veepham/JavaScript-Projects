1f79da60d2a0b982ff0ea2a362adc354ca849d6f
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 16:23:59 -0700

    Boolean assignment
    greater/less than

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,13 +1,11 @@
-document.write("20" + 4);
-
-function my_Function() {
+function my_Function() {        //NaN//
     document.getElementById("Test").innerHTML = 0/0;
 }
-function my_Function2() {
+function my_Function2() {       //Checking if something is NaN, true//
     document.getElementById("Test2").innerHTML = isNaN('This is a string');
 }
 function my_Function3() {
-    document.getElementById("Test3").innerHTML = isNaN('123');
+    document.getElementById("Test3").innerHTML = isNaN('123');       //Checking if something is NaN, false//
 }
 
-document.write(-2E310);
+document.write(100<2);
