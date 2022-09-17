159c1a569f89a3410c54f54dd2c0d0bcf2970203
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 14:23:32 -0700

    Modulus Operator

--- Basic JavaScript Projects/Project3_math_operations/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operations/JS/main.js
@@ -18,3 +18,7 @@ function more_Math(){
     var math = (1 + 2) * 10 / 2 - 5;
     document.getElementById("maths").innerHTML = "1 plus 2, multiplies by 10, divided in hald and then subtracted by 5 equals " + math;
 }
+function modulus_Operator() {
+    var modulus_Math = 25 % 6;
+    document.getElementById("mod_math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + modulus_Math;
+}
