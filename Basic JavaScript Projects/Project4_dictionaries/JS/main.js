32090d036cf19b6cb88d6365daca7eb196ebc4ca
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 15:33:13 -0700

    Notating code

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,12 +1,12 @@
-function my_Dictionary(){
+function my_Dictionary(){           //creating a dictionary//
     var Animal ={
         Species: "Dog",
         Color: "Tan",
         Breed: "Havanese Mix",
         Age: 10,
         Sound: "Woof!",
-        Sound: "Bark!"
+        Sound: "Bark!"            //adding an identical key - it showed this secondary value//
     };
-    delete Animal.Sound;
+    delete Animal.Sound;        //deleting a KVP - result will show undefined//
     document.getElementById("Dictionary").innerHTML = Animal.Sound;
 }
