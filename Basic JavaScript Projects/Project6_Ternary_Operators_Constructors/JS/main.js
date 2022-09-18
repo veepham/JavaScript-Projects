abef034b0ebec108b577a212d8941bedcc6f653c
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 20:42:57 -0700

    Ternary and Keywords assignment

---
+++ Basic JavaScript Projects/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -0,0 +1,19 @@
+function Vote_Function(){       //ternary//
+    var Age, Can_Vote;
+    Age = document.getElementById("Age").value;
+    Can_Vote = (Age < 18) ? "You are too young": "You are old enough";
+    document.getElementById("Vote").innerHTML = Can_Vote + " to vote.";
+}
+
+function Vehicle(Make, Model, Year, Color){         //Keywords "new" "this"//
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction(){
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
