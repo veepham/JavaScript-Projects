c524b95712999bc57c2c047e6d6b8dadb205b163
veepham <111474183+veepham@users.noreply.github.com>
2022-09-18 16:40:32 -0700

    uppercase and search methods

--- Basic JavaScript Projects/Project8_String_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_String_methods/JS/main.js
@@ -1,4 +1,4 @@
-function full_sentence(){
+function full_sentence(){       //concat method//
     var part_1 = "I have ";
     var part_2 = "made this ";
     var part_3 = "into a complete ";
@@ -6,8 +6,17 @@ function full_sentence(){
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
-function slice_Method(){
+function slice_Method(){        //slice method//
     var Sentence = "All work and no play makes Johnny a dull boy.";
     var section = Sentence.slice(27,33);
     document.getElementById("slice").innerHTML = section;
+}
+function myFunction(){      //toUpperCase method//
+    let text = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = text.toUpperCase();
+}
+
+function mySearch(){        //search method//
+let str = "Please locate the word 'dog' in this sentence";      
+document.getElementById("search").innerHTML = str.search("dog");
 }
