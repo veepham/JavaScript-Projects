37d097d25ce1dcb095279457a2c6c4d9d75b40e1
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 20:01:56 -0700

    Project 5

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,12 +1,27 @@
+document.write(typeof 3);       //typeof operator//
+
+function coercion() {       //type coercion of a string and number//
+    document.getElementById("coercion").innerHTML = "10" + 5
+}
 function my_Function() {        //NaN//
     document.getElementById("Test").innerHTML = 0/0;
 }
 function my_Function2() {       //Checking if something is NaN, true//
     document.getElementById("Test2").innerHTML = isNaN('This is a string');
 }
-function my_Function3() {
-    document.getElementById("Test3").innerHTML = isNaN('123');       //Checking if something is NaN, false//
+function my_Function3() {       //Checking if something is NaN, false//
+    document.getElementById("Test3").innerHTML = isNaN('123');       
 }
 
-document.write(100<2);
-console.log(2 > 2);
+function my_Function4(){        //utilizing greater than, less than, AND//
+    document.getElementById("Test4").innerHTML = 100>2 && 5<10;
+}
+function my_Function5(){        //utilizing == and OR//
+    document.getElementById("Test5").innerHTML = 2==2 || 10<1;
+}
+function my_Function6(){        //utilizing  ===//
+    document.getElementById("test6").innerHTML = ("2") === 2;
+}
+function not_Function(){
+    document.getElementById("not").innerHTML = !(20 < 10);
+}
