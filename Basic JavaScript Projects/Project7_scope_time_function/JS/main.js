fa9cb6a5462ac07b0b2697a1fb3bc8e94fc795e1
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 22:19:44 -0700

    else if statement

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -19,7 +19,7 @@ function Add_4(){
 Add_3();
 Add_4();
 
-function get_Date(){
+function get_Date(){        //utilizing a date method//
     if (new Date().getHours() < 18){
         document.getElementById("Greeting").innerHTML = "How are you today?";
     }
@@ -31,7 +31,7 @@ function myFunction(){
     }
 }
 
-function Age_Function(){
+function Age_Function(){        //utilizing else statement//
     Age = document.getElementById("Age").value;
     if (Age >= 21){
         Alcohol = "You are old enough to buy alcohol.";
@@ -40,4 +40,19 @@ function Age_Function(){
         Alcohol = "You are not old enough to buy alcohol.";
     }
     document.getElementById("How_old").innerHTML = Alcohol;
+}
+
+function Time_Function(){       //utilizing else if statement//
+    var Time = new Date().getDay();
+    var Reply;
+    if (Time < 2 == Time > 0) {
+        Reply = "The week is just starting!";
+    }
+    else if (Time >= 3 == Time < 5){
+        Reply = "We are in mid-week.";
+    }
+    else {
+        Reply = "Happy weekend!";
+    }
+    document.getElementById("What_Day").innerHTML = Reply;
 }
