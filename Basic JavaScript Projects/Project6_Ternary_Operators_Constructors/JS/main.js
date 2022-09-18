81686448f0be8d9200990d7d26c94e907da54805
veepham <111474183+veepham@users.noreply.github.com>
2022-09-17 21:24:18 -0700

    nested function

--- Basic JavaScript Projects/Project6_Ternary_Operators_Constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -17,3 +17,24 @@ var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction(){
     document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
+
+function Dog(Color, Breed, Age){        //creating my own object constructor//
+    this.Dog_Color = Color;
+    this.Dog_Breed = Breed;
+    this.Dog_Age = Age;
+}
+var Mup = new Dog("Tan and White", "Havanese Mix", 10)
+function myFunction2(){
+    document.getElementById("New_and_This").innerHTML = "My dog is a " + Mup.Dog_Age + " year old " + Mup.Dog_Breed;
+}
+
+
+function count_Function(){      //nested function//
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count(){
+        var starting_point = 4;
+        function Plus_two() {starting_point += 2;}
+        Plus_two();
+        return starting_point;
+    }
+}
