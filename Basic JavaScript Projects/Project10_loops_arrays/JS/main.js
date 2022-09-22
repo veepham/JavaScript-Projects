844c4f949679068930ed4311d1bbfc4b2d7a5847
veepham <111474183+veepham@users.noreply.github.com>
2022-09-22 12:51:06 -0700

    Displaying a value in array

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,29 @@
+function Call_Loop(){       //while loop//
+    var Digit ="";
+    var X = 1;
+    while (X<11){
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+function String_Length(){       //length property//
+    let text = "Hello";
+    let length = text.length;
+    document.getElementById("length").innerHTML = length;
+}
+
+//for loop//
+var Cities = ["Portland", "Los Angeles", "Philadelphia", "New York City", "Las Vegas", "Cleveland", "Austin"];
+var Content ="";
+var Y;
+function for_Loop(){
+    for (Y = 0; Y < Cities.length; Y++){
+        Content += Cities[Y] + "<br>";
+    }
+    document.getElementById("List_Cities").innerHTML = Content;
+}
+function array_Function(){
+    document.getElementById("Array").innerHTML = "The next city I am travelling to will be " + Cities[1];
+}
