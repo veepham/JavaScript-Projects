8bf3751bd62a6a7246b6fbe4826fd9d20d4a00cf
veepham <111474183+veepham@users.noreply.github.com>
2022-09-22 20:21:27 -0700

    Linear gradient

--- Basic JavaScript Projects/Project11/JS/main.js
+++ Basic JavaScript Projects/Project11/JS/main.js
@@ -6,3 +6,9 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.font = "30px Tahoma";
 ctx.strokeText("Hello World", 10, 50);
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(10,100,200,100);
