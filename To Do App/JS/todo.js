4ab10633f821016343d986d99c7c76300f4c0067
veepham <111474183+veepham@users.noreply.github.com>
2022-10-09 16:19:36 -0700

    Javascript code for To Do List
    Adding task from input, new task into todo list array, keeping tasks permanently displayed, creating X button for removal

---
+++ To Do App/JS/todo.js
@@ -0,0 +1,38 @@
+/* getting task from input */
+function get_todos(){
+    var todos = new Array;      /* creates array from input*/
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null){
+        todos=JSON.parse(todos_str);
+    }
+    return todos;
+}
+
+/* function to add inputted task to the get_todos array */
+function add(){
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    document.getElementById('task').value="";
+    show();
+
+    return false;
+}
+
+/*function for keeping tasks permanently displayed*/
+function show(){
+    var todos = get_todos();
+
+    var html = '<ul>';
+    for (var i = 0; i < todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id=" '+ i + '">x</button></li>';
+
+    };
+    html += '</ul>';
+    document.getElementById('todos').innerHTML = html;
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
